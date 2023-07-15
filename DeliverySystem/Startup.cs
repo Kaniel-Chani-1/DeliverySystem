93ba45528894ec2ab4c62aee4a59ae14834ee58b
Kaniel-Chani-1 <73110664+Kaniel-Chani-1@users.noreply.github.com>
2023-07-16 01:30:30 +0300

    Add DeliverySystem folder

---
+++ DeliverySystem/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AppServices;
+using Microsoft.Extensions.Configuration;
+
+namespace DeliverySystem
+{
+    public class Startup
+    {
+        IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddServices(configuration);
+            services.AddCors(s => s.AddDefaultPolicy(
+            builder => builder.WithOrigins("http://localhost:3000")
+           .AllowAnyHeader()//content-type, etc.
+           .AllowAnyMethod()));//only get, allow get and post
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseRouting();
+            app.UseCors();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
